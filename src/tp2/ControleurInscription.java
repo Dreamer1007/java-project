28bb9cedb2be2b6911bf3e8f8ac22bf6a3461c5d
Dreamer1007 <meriem.batata53@gmail.com>
2021-03-06 03:41:16 +0100

    commit 24: implementer la classe ControlleurInscription

---
+++ src/tp2/ControleurInscription.java
@@ -0,0 +1,20 @@
+package tp2;
+
+import java.sql.SQLException;
+
+public class ControleurInscription {
+	
+	public void inscriptionControl (int matricule, String nom, String prénom, String email,String pwd, int id_universite) throws SQLException
+	{
+		EtudiantService serv=new EtudiantService();
+	
+	try {
+		
+		serv.inscription(899, "batata", "batata", "merbatata@gmail.com","xxxxxxx", 1);
+		
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+	}
--- src/tp2/MainApp.java
+++ src/tp2/MainApp.java
@@ -10,7 +10,7 @@ public class MainApp {
 		EtudiantService serv=new EtudiantService();
 		try {
 			
-			serv.inscription(8, "batata", "batata", "batata@gmail.com","xxxxxxx", 1);
+			serv.inscription(899, "batata", "batata", "batata@gmail.com","xxxxxxx", 1);
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
